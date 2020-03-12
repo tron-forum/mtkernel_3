8ddc64ace897503724d097c0a32f4635c8b94054
tron-forum <tkernel_dev@tron.org>
2020-03-12 16:52:01 +0900

    Documentation and version number updates

--- kernel/sysdepend/iote_rx231/devinit.c
+++ kernel/sysdepend/iote_rx231/devinit.c
@@ -1,12 +1,12 @@
 /*
  *----------------------------------------------------------------------
- *    micro T-Kernel 3.00.B1
+ *    micro T-Kernel 3.00.00.B1
  *
  *    Copyright (C) 2006-2019 by Ken Sakamura.
  *    This software is distributed under the T-License 2.1.
  *----------------------------------------------------------------------
  *
- *    Released by TRON Forum(http://www.tron.org) at 2020/03
+ *    Released by TRON Forum(http://www.tron.org) at 2020/03/13
  *----------------------------------------------------------------------
  */
 #include <sys/machine.h>
