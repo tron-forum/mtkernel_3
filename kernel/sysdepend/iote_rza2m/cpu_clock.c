c040753e6e4eab688adc1587e2f6ed7ee3c9a478
Yuichi Toyoyama <yuichi.toyoyama@iniad.org>
2022-10-07 14:00:24 +0900

    RZ/A2M cpu_clock.c移動 & 割込み関係の定義変更

--- kernel/sysdepend/iote_rza2m/cpu_clock.c
+++ kernel/sysdepend/iote_rza2m/cpu_clock.c
@@ -1,12 +1,12 @@
 /*
  *----------------------------------------------------------------------
- *    micro T-Kernel 3.00.05
+ *    micro T-Kernel 3.00.06
  *
- *    Copyright (C) 2006-2021 by Ken Sakamura.
+ *    Copyright (C) 2006-2022 by Ken Sakamura.
  *    This software is distributed under the T-License 2.2.
  *----------------------------------------------------------------------
  *
- *    Released by TRON Forum(http://www.tron.org) at 2021/11.
+ *    Released by TRON Forum(http://www.tron.org) at 2022/10.
  *
  *----------------------------------------------------------------------
  */
