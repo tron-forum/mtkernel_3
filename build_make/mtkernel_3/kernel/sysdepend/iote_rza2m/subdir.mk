07577a4d297c9cbbfd1220f71460cb9ce9262679
Yuichi Toyoyama <yuichi.toyoyama@iniad.org>
2023-03-02 14:58:15 +0900

    Makefile修正

--- build_make/mtkernel_3/kernel/sysdepend/iote_rza2m/subdir.mk
+++ build_make/mtkernel_3/kernel/sysdepend/iote_rza2m/subdir.mk
@@ -3,11 +3,13 @@
 ################################################################################
 
 OBJS += \
+./mtkernel_3/kernel/sysdepend/iote_rza2m/cpu_clock.o \
 ./mtkernel_3/kernel/sysdepend/iote_rza2m/hw_setting.o \
 ./mtkernel_3/kernel/sysdepend/iote_rza2m/devinit.o \
 ./mtkernel_3/kernel/sysdepend/iote_rza2m/power_save.o 
 
 C_DEPS += \
+./mtkernel_3/kernel/sysdepend/iote_rza2m/cpu_clock.d \
 ./mtkernel_3/kernel/sysdepend/iote_rza2m/hw_setting.d \
 ./mtkernel_3/kernel/sysdepend/iote_rza2m/devinit.d \
 ./mtkernel_3/kernel/sysdepend/iote_rza2m/power_save.d 
@@ -21,6 +23,6 @@ mtkernel_3/kernel/sysdepend/iote_rza2m/%.o: ../kernel/sysdepend/iote_rza2m/%.c
 
 mtkernel_3/kernel/sysdepend/iote_rza2m/%.o: ../kernel/sysdepend/iote_rza2m/%.S
 	@echo 'Building file: $<'
-	$(GCC) $(CFLAGS) -D$(TARGET) $(INCPATH) -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	$(AS) $(ASFLAGS) -D$(TARGET) $(INCPATH) -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
