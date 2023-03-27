762e17234ad4aba7aad9766e2339f17a7fb1e615
Yuichi Toyoyama <yuichi.toyoyama@iniad.org>
2023-03-27 17:51:11 +0900

    コメント日付更新

--- kernel/tkernel/timer.c
+++ kernel/tkernel/timer.c
@@ -1,12 +1,12 @@
 /*
  *----------------------------------------------------------------------
- *    micro T-Kernel 3.00.00
+ *    micro T-Kernel 3.00.06A
  *
- *    Copyright (C) 2006-2019 by Ken Sakamura.
- *    This software is distributed under the T-License 2.1.
+ *    Copyright (C) 2006-2023 by Ken Sakamura.
+ *    This software is distributed under the T-License 2.2.
  *----------------------------------------------------------------------
  *
- *    Released by TRON Forum(http://www.tron.org) at 2019/12/11.
+ *    Released by TRON Forum(http://www.tron.org) at 2023/03.
  *
  *----------------------------------------------------------------------
  */
