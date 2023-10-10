153cdce35491a33cd55a88a8649dd5781585f7fd
Yuichi Toyoyama <yuichi.toyoyama@iniad.org>
2023-10-10 19:37:29 +0900

    潜在的不具合の修正

--- kernel/sysdepend/cpu/core/armv7m/dispatch.S
+++ kernel/sysdepend/cpu/core/armv7m/dispatch.S
@@ -1,12 +1,12 @@
 /*
  *----------------------------------------------------------------------
- *    micro T-Kernel 3.00.06A
+ *    micro T-Kernel 3.00.07.B0
  *
  *    Copyright (C) 2006-2023 by Ken Sakamura.
  *    This software is distributed under the T-License 2.2.
  *----------------------------------------------------------------------
  *
- *    Released by TRON Forum(http://www.tron.org) at 2023/03.
+ *    Released by TRON Forum(http://www.tron.org) at 2023/11.
  *
  *----------------------------------------------------------------------
  */
