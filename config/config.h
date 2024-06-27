7977288c90914974be666c91f1f0370686d2abc8
fujita <fujita@personal-media.co.jp>
2024-06-27 14:04:17 +0900

    一時スタックサイズ変更
    config/config.h: #define CNF_TMP_STACK_SIZE (256) → (2048)
    一時スタックは、割込ハンドラで使用されることがあるので、256 は少なすぎる。
    割込ハンドラで使用されることを考慮し、CNF_EXC_STACK_SIZE と同じ 2048 とした。

--- config/config.h
+++ config/config.h
@@ -81,7 +81,7 @@
 /* Stack size definition
  */
 #define CNF_EXC_STACK_SIZE	(2048)	/* Exception stack size */
-#define	CNF_TMP_STACK_SIZE	(256)	/* Temporary stack size */
+#define	CNF_TMP_STACK_SIZE	(2048)	/* Temporary stack size */
 
 
 /*---------------------------------------------------------------------- */
